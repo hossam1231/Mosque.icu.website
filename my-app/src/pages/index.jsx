ad93d74df55a55cc13652fe2ebb1abfe90351a97
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-01-13 17:17:23 +0000

    init

---
+++ my-app/src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { Reviews } from '@/components/Reviews'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mosque.icu - Invest in the Akhira one step at a time.</title>
+        <meta
+          name="description"
+          content="By promoting institutions from our network, you’ll know exactly when to pray, where to pray and what you missed out on."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Reviews />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
