aa13dfd62f12f6f7823b1fd01191cb594e8cec80
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-03-03 18:07:37 +0000

    push Co-authored-by: Mitch <MVG47@users.noreply.github.com>

--- my-app/src/data/URL.js
+++ my-app/src/data/URL.js
@@ -1,6 +1,6 @@
 // dev
-export const websiteURL = "http://localhost:8910";
-export const apiURL = "http://localhost:8911";
+export const websiteURL = 'https://web.mosque.icu'
+export const apiURL = 'https://web.mosque.icu'
 // production
 // export const websiteURL = "http://localhost:8910";
 // export const apiURL = "http://localhost:8911";
